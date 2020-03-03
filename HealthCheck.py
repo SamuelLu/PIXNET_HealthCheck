5ee165d52f66534c90041a183a0c4ea0cfd4238c
Samuel Lu <SamuelLu@users.noreply.github.com>
2020-03-03 16:37:46 +0800

    # coding=Big5

--- HealthCheck.py
+++ HealthCheck.py
@@ -1,5 +1,6 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
+# coding=Big5
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
